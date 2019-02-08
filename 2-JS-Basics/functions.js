92b7a32c22c02f47a8342bde510309c353947ab2
Nelson Huezo <nelson.huezo88@gmail.com>
2019-02-08 17:56:56 -0500

    Works on JS functions
    - Worked on js functions and practiced making some simple functions

--- 2-JS-Basics/functions.js
+++ 2-JS-Basics/functions.js
@@ -1,12 +1,16 @@
-// lecture: functions
+/*********************************************************************
+VIDEO: FUNCTIONS
+*/
 
+/*
 const calculateAge = function (yearOfBirth){
-  let age = 2018 - yearOfBirth
+  let age = 2019 - yearOfBirth
   return age
 }
 
 let ageNelson = calculateAge(1988)
 let ageMike = calculateAge(1969)
+console.log(ageNelson)
 console.log(ageMike)
 
 
@@ -22,3 +26,35 @@ console.log(name + ' can retire in ' + retirement + ' years!')
 
 yearsUntilRetirement('Nelson', 1988)
 yearsUntilRetirement('Mary', 1948)
+*/
+
+/*********************************************************************
+VIDEO: FUNCTION STATEMENTS AND EXPRESSIONS
+*/
+
+// fucntion expression
+// function statement
+    // function someFun(par) {
+    //code
+    // }
+// Function expression
+    // const someFun = function(par) {
+    //code
+    // }
+const whatDoYouDo = function(job, firstName) {
+  switch (job) {
+    case 'teacher':
+      return firstName + ' teaches the streets!'
+    case 'designer':
+      return firstName + ' designs for the streets!'
+    case 'web developer':
+      return firstName + ' develops for the streets!'
+    default:
+      return firstName + ' is a bum and has no job'
+  }
+}
+
+console.log(whatDoYouDo('web developer', 'Papi'))
+console.log(whatDoYouDo('designer', 'Papi'))
+console.log(whatDoYouDo('teacher', 'Papi'))
+console.log(whatDoYouDo('pharmacist', 'Papi'))
