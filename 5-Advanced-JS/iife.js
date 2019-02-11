8d7d2210076ce09dae5cf6b76505c95533d3704b
Nelson Huezo <nelson.huezo88@gmail.com>
2019-02-11 13:06:21 -0500

    Makes a title edit on the top of the iify file

--- 5-Advanced-JS/iife.js
+++ 5-Advanced-JS/iife.js
@@ -3,16 +3,16 @@
 */
 
 // iife
+/*
 
 function game() {
   var score = Math.random * 10
   console.log(score >= 5)
 }
 game()
-/*
+*/
 
 (function () {
   var score = Math.random * 10
   console.log(score >= 5)
 })()
-*/
